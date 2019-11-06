c6ce3422d8398b2bce9a65224f9cf0f1bf8fa3df
Patrick Pokatilo <mail@shyxormz.net>
2019-11-06 02:33:52 +0100

    /users/{name}

--- pkg/cacoo/users.go
+++ pkg/cacoo/users.go
@@ -1,1 +1,18 @@
 package cacoo
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func NewUsersRequest(ctx context.Context, name string) (*http.Request, error) {
+	return http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("https://cacoo.com/api/v1/users/%s.xml", name), nil)
+}
+
+type UsersResponse struct {
+	Name     string `xml:"name"`
+	Nickname string `xml:"nickname"`
+	Type     string `xml:"type"`
+	ImageURL string `xml:"imageUrl"`
+}
