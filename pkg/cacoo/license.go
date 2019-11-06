be8f5e9277630f6967ea37998da751a4c26bc0e3
Patrick Pokatilo <mail@shyxormz.net>
2019-11-06 02:33:37 +0100

    /account/license

--- pkg/cacoo/license.go
+++ pkg/cacoo/license.go
@@ -1,1 +1,21 @@
 package cacoo
+
+import (
+	"context"
+	"net/http"
+)
+
+func NewLicenseRequest(ctx context.Context) (*http.Request, error) {
+	return http.NewRequestWithContext(ctx, http.MethodGet, "https://cacoo.com/api/v1/account/license.xml", nil)
+}
+
+type LicenseResponse struct {
+	Plan                              string `xml:"plan"`
+	RemainingSheets                   int    `xml:"remainingSheets"`
+	RemainingSharedFolders            int    `xml:"remainingSharedFolders"`
+	MaxNumberOfSharersPerDiagram      int    `xml:"maxNumberOfSharersPerDiagram"`
+	MaxNumberOfSharersPerSharedFolder int    `xml:"maxNumberOfSharersPerSharedFolder"`
+	CanCreateSheet                    bool   `xml:"canCreateSheet"`
+	CanCreateSharedFolder             bool   `xml:"canCreateSharedFolder"`
+	CanExportVectorFormat             bool   `xml:"canExportVectorFormat"`
+}
