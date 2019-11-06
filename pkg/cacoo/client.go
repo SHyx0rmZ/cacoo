97d64891a8f298aeac9bfe4e4820ddf6bc46c97b
Patrick Pokatilo <mail@shyxormz.net>
2019-11-06 02:34:10 +0100

    NewClient()

--- pkg/cacoo/client.go
+++ pkg/cacoo/client.go
@@ -1,1 +1,25 @@
 package cacoo
+
+import (
+	"net/http"
+)
+
+type Client struct {
+	client *http.Client
+	apiKey string
+}
+
+func NewClient(apiKey string) *Client {
+	return &Client{
+		client: http.DefaultClient,
+		apiKey: apiKey,
+	}
+}
+
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	req2 := req.Clone(req.Context())
+	q := req2.URL.Query()
+	q.Add("apiKey", c.apiKey)
+	req2.URL.RawQuery = q.Encode()
+	return c.client.Do(req2)
+}
